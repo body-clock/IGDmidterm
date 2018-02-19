2f53923cf58c20cc7ce8083137ad69bf84143d87
Patrick Perkins <plp248@nyu.edu>
2018-02-19 17:51:06 -0500

    added rough snowboarding mechanic down a hill - and rail

---
+++ dislocation/Assets/TextController.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TextController : MonoBehaviour
+{
+
+	public int timer = 0;
+
+	void Start()
+	{
+		timer = 0;
+	}
+
+	void OnEnable()
+	{
+		
+	}
+}
---
+++ dislocation/Assets/characterController.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class characterController : MonoBehaviour
+{
+
+	public float speed = 10f;
+	public float jumpForce = 200f;
+	public Rigidbody rb;
+
+	// Use this for initialization
+	void Start () {
+		Cursor.lockState = CursorLockMode.Locked;
+		rb = GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		float translation = Input.GetAxis("Vertical")* speed * Time.deltaTime;
+		float straffe = Input.GetAxis("Horizontal") * speed * Time.deltaTime;
+		
+		transform.Translate(straffe, 0, translation);
+
+		if (Input.GetKeyDown("escape"))
+		{
+			Cursor.lockState = CursorLockMode.None;
+		}
+
+		if (Input.GetKeyDown(KeyCode.Space))
+		{
+			rb.AddForce(Vector3.up * jumpForce);
+			print("jump attempt");
+		}
+	}
+}
