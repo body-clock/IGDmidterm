5d356d59bb59c967831342079e9c6746587f1e82
Patrick Perkins <plp248@nyu.edu>
2018-02-20 00:25:06 -0500

    rough proto

--- dislocation/Assets/MakePlayerFall.cs
+++ dislocation/Assets/MakePlayerFall.cs
@@ -2,13 +2,17 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class MakePlayerFall : MonoBehaviour {
+public class MakePlayerFall : MonoBehaviour
+{
+
+    public GameObject uiText;
 
     void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.CompareTag("Player"))
         {
             other.gameObject.GetComponent<Rigidbody>().freezeRotation = false;
+            uiText.SetActive(true);
         }
     }
 }
--- dislocation/Assets/TextController.cs
+++ dislocation/Assets/TextController.cs
@@ -1,19 +1,44 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class TextController : MonoBehaviour
 {
 
-	public int timer = 0;
+	public float timer = 0;
+	public Text storyText;
 
 	void Start()
 	{
 		timer = 0;
+		storyText = GetComponent<Text>();
 	}
 
-	void OnEnable()
+	void Update()
 	{
+		timer += Time.deltaTime;
+		print(timer);
+
+		if (timer > 4f)
+		{
+			storyText.text = "My shoulder... Feels out of place...";
+		}
 		
+		if (timer > 8f)
+		{
+			storyText.text = "I think I may have... DISLOCATED IT.";
+		}
+
+		if (timer > 12f)
+		{
+			storyText.text = "My shoulder, that is.";
+		}
+
+		if (timer > 16f)
+		{
+			SceneManager.LoadScene("cabin");
+		}
 	}
 }
